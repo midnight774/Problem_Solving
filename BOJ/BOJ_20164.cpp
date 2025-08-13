676bdea4c09e62437715ddb71dfeb20bbb51369b
Jaeheon Yu <101785647+midnight774@users.noreply.github.com>
2025-08-13 19:21:39 +0900

    Create BOJ_20164.cpp

---
+++ BOJ/BOJ_20164.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/*
+BOJ 20164 홀수 홀릭 호석
+풀이 방법 : 재귀
+*/
+
+string Input;
+int Min = 15;
+int Max = 0;
+
+void CheckOdd(string Input, int Cnt)
+{
+	int Length = Input.length();
+
+	int OddCnt = 0;
+	for (int i = 0; i < Length; ++i)
+	{
+		if ((int)(Input[i] - '0') % 2 == 1)
+			++OddCnt;
+	}
+
+	if (Input.length() == 1)
+	{
+		Min = min(Cnt + OddCnt, Min);
+		Max = max(Cnt + OddCnt, Max);
+		return;
+	}
+
+	else if(Input.length() == 2)
+	{
+		int Num = (int)(Input[0] - '0') + (int)(Input[1] - '0');
+		string Next = to_string(Num);
+		CheckOdd(Next, Cnt + OddCnt);
+	}
+
+	else
+	{
+		for (int i = 1; i < Length; ++i)
+		{
+			for (int j = i + 1; j < Length; ++j)
+			{
+				int Num = stoi(Input.substr(0, i))
+					+ stoi(Input.substr(i, j - i))
+					+ stoi(Input.substr(j, Length - j));
+
+				string Next = to_string(Num);
+
+				CheckOdd(Next, Cnt + OddCnt);
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	cin >> Input;
+	CheckOdd(Input, 0);
+
+	cout << Min << ' ' << Max;
+
+}
+
